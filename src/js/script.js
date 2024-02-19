0ef2b3e74cd2146c54e2af1eda4fec1a81bb6c78
petersimmalugnt <peter@simmalugnt.se>
2024-02-19 11:03:08 +0100

    Automatisk commit: sparade ändringar

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-import { dualRangeSlider } from "./modules/_dualRangeSlider";
-import { inViewSticky } from "./modules/_inViewSticky";
+import { dualRangeSlider } from "./modules/DualRangeSlider";
+import { inViewSticky } from "./modules/InViewSticky";
 
 document.addEventListener("DOMContentLoaded", () => {
   inViewSticky();
