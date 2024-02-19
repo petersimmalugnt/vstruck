dd4a92b86f2cfb64c649ca81c6ab401314f31d0a
petersimmalugnt <peter@simmalugnt.se>
2024-02-19 09:54:08 +0100

    modified 6 files

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-import{dualRangeSlider}from"./modules/dualRangeSlider";import{inViewSticky}from"./modules/inViewSticky";document.addEventListener("DOMContentLoaded",(()=>{inViewSticky(),dualRangeSlider(0,400,100,300)}));
+"use strict";var _dualRangeSlider=require("./modules/dualRangeSlider"),_inViewSticky=require("./modules/inViewSticky");document.addEventListener("DOMContentLoaded",(function(){(0,_inViewSticky.inViewSticky)(),(0,_dualRangeSlider.dualRangeSlider)(0,400,100,300)}));
--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,59 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const rename = require('gulp-rename');
-const terser = require('gulp-terser');
-const browserSync = require('browser-sync').create();
-const del = require('del');
-const git = require('gulp-git');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const rename = require("gulp-rename");
+const terser = require("gulp-terser");
+const browserSync = require("browser-sync").create();
+const del = require("del");
+const git = require("gulp-git");
+const babel = require("gulp-babel");
 
 function clean() {
-  return del(['dist/*']);
+  return del(["dist/*"]);
 }
 
 function styles() {
-  return gulp.src('src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp
+    .src("src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest("dist/css"))
     .pipe(postcss([cssnano()]))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 }
 
 function scripts() {
-  return gulp.src('src/js/**/*.js')
+  return gulp
+    .src("src/js/**/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-transform-modules-commonjs"],
+      })
+    )
     .pipe(terser())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest("dist/js"))
     .pipe(browserSync.stream());
 }
 
 function copyHtml() {
-  return gulp.src('src/*.html')
-    .pipe(gulp.dest('dist'))
+  return gulp
+    .src("src/*.html")
+    .pipe(gulp.dest("dist"))
     .pipe(browserSync.stream());
 }
 
 function autoGitCommit() {
-  return gulp.src('./')
-    .pipe(git.add({ args: '-A' }))
-    .pipe(git.commit('Automatisk commit: sparade ändringar'))
-    .on('end', function() {
-      git.push('origin', 'main', function(err) {
+  return gulp
+    .src("./")
+    .pipe(git.add({ args: "-A" }))
+    .pipe(git.commit("Automatisk commit: sparade ändringar"))
+    .on("end", function () {
+      git.push("origin", "main", function (err) {
         if (err) {
           console.error("Error pushing to Git:", err);
           throw err;
@@ -53,7 +64,7 @@ function autoGitCommit() {
     });
 }
 
-const { promisify } = require('util');
+const { promisify } = require("util");
 const setTimeoutPromise = promisify(setTimeout);
 
 function delayedGitCommit() {
@@ -63,14 +74,18 @@ function delayedGitCommit() {
 function watch() {
   browserSync.init({
     server: {
-      baseDir: './dist'
-    }
+      baseDir: "./dist",
+    },
   });
-  gulp.watch('src/sass/**/*.scss', gulp.series(styles, delayedGitCommit));
-  gulp.watch('src/js/**/*.js', gulp.series(scripts, delayedGitCommit));
-  gulp.watch('src/*.html', gulp.series(copyHtml, delayedGitCommit));
+  gulp.watch("src/sass/**/*.scss", gulp.series(styles, delayedGitCommit));
+  gulp.watch("src/js/**/*.js", gulp.series(scripts, delayedGitCommit));
+  gulp.watch("src/*.html", gulp.series(copyHtml, delayedGitCommit));
 }
 
-const buildAndServe = gulp.series(clean, gulp.parallel(styles, scripts, copyHtml), watch);
+const buildAndServe = gulp.series(
+  clean,
+  gulp.parallel(styles, scripts, copyHtml),
+  watch
+);
 
-gulp.task('default', buildAndServe);
+gulp.task("default", buildAndServe);
