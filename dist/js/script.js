21530ce92f3e61f4d5d4f3b72edf7449a1190d4a
petersimmalugnt <peter@simmalugnt.se>
2024-02-19 09:39:01 +0100

    Automatisk commit: sparade ändringar

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-import{dualRangeSlider}from"./modules/dualRangeSlider";document.addEventListener("DOMContentLoaded",(()=>{dualRangeSlider(0,400,100,300)}));
+import{dualRangeSlider}from"./modules/dualRangeSlider";import{inViewSticky}from"./modules/inViewSticky";document.addEventListener("DOMContentLoaded",(()=>{inViewSticky(),dualRangeSlider(0,400,100,300)}));
--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,7 @@
 import { dualRangeSlider } from "./modules/dualRangeSlider";
+import { inViewSticky } from "./modules/inViewSticky";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const inViewSticky = () => {
-    const elements = [
-      ...document.querySelectorAll(
-        '[data-scroll-inview="true"], [data-inview-target]:not([data-inview-target=""])'
-      ),
-    ];
-    const grouped = elements.reduce((a, e) => {
-      const t = e.getAttribute("data-inview-threshold") || "0";
-      (a[t] = a[t] || []).push(e);
-      return a;
-    }, {});
-
-    Object.entries(grouped).forEach(([threshold, els]) => {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const targets =
-              document.querySelectorAll(
-                entry.target.getAttribute("data-inview-target")
-              ) || [];
-            const isIntersecting = entry.isIntersecting;
-            const isPassed = entry.boundingClientRect.y <= 0 && !isIntersecting;
-
-            [entry.target, ...targets].forEach((t) => {
-              ["passed", "inview", "notinview"].forEach((c) =>
-                t.classList.toggle(
-                  c,
-                  c === "passed"
-                    ? isPassed
-                    : c === "inview"
-                    ? isIntersecting
-                    : !isIntersecting
-                )
-              );
-            });
-          });
-        },
-        { root: null, rootMargin: `0px`, threshold: parseFloat(threshold) }
-      );
-
-      els.forEach((e) => observer.observe(e));
-    });
-  };
-
+  inViewSticky();
   dualRangeSlider(0, 400, 100, 300);
 });
