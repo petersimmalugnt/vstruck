93646d947959db79e4f5e6ae75937019f462e43f
petersimmalugnt <peter@simmalugnt.se>
2024-02-27 17:39:51 +0100

    modified script.js and script.js in webflow

--- src/js/webflow/script.js
+++ src/js/webflow/script.js
@@ -138,16 +138,13 @@ const allFilterDdToggle = () => {
 };
 
 const dropDowns = () => {
-  document
-    .querySelectorAll(".dropdown-toggle-container")
-    .array.forEach((btn) => {
-      btn.addEventListener(
-        "click",
-        () =>
-          (btn.dataset.ddOpen =
-            btn.dataset.ddOpen === "true" ? "false" : "true")
-      );
-    });
+  document.querySelectorAll(".dropdown-toggle-container").forEach((btn) => {
+    btn.addEventListener(
+      "click",
+      () =>
+        (btn.dataset.ddOpen = btn.dataset.ddOpen === "true" ? "false" : "true")
+    );
+  });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
