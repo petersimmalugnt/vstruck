cc58d2c202379596bdfa8b3d35948d997943f604
petersimmalugnt <peter@simmalugnt.se>
2024-03-19 19:02:02 +0100

    modified script.js and script.js in webflow

--- src/js/webflow/script.js
+++ src/js/webflow/script.js
@@ -300,7 +300,7 @@ const truckSingelImagesScroll = () => {
             return;
           }
 
-          function easeOutCubic(x: number): number {
+          function easeOutCubic(x) {
             return 1 - Math.pow(1 - x, 3);
           }
 
