ef028e3ba774ad6037ae2746a757f567152c8bcb
petersimmalugnt <peter@simmalugnt.se>
2024-03-19 19:23:44 +0100

    modified script.js and script.js in webflow

--- src/js/webflow/script.js
+++ src/js/webflow/script.js
@@ -297,7 +297,7 @@ const truckSingelImagesScroll = () => {
 
         const scrollLoop = () => {
           console.log("Cur: " + Math.abs(currentProgress - 1));
-          if (Math.abs(currentProgress - 1) < 0.00000000001) {
+          if (currentProgress > 0.9999999999) {
             wrapper.scrollLeft = targetPos + scrollPos;
             return;
           }
