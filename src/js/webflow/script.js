321346179f3ed95d60e9eeea24c4b17a36f9a30a
petersimmalugnt <peter@simmalugnt.se>
2024-03-25 19:15:22 +0100

    modified script.js and script.js in webflow

--- src/js/webflow/script.js
+++ src/js/webflow/script.js
@@ -364,7 +364,7 @@ const singelTruckFinances = () => {
     const radioWrapper = item.querySelector(".stf-radio");
     if (!radioWrapper) return;
     radioWrapper
-      .querySelectorAll("stf-radio-selector")
+      .querySelectorAll(".stf-radio-selector")
       .forEach((radioSelect, i) => {
         radioSelect.addEventListener("click", () => {
           console.log(i);
